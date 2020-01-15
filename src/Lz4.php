d4d13ee2a0ba216cc3e6a077233a7c73a59e60a3
dexen deVries <dexen.devries@gmail.com>
2020-01-15 13:59:27 +0100

    the decompression loop

--- src/Lz4.php
+++ src/Lz4.php
@@ -3,11 +3,22 @@
 namespace dexen\mulib;
 
 use dexen\mulib\Lz4\Frame;
+use dexen\mulib\Lz4\DataBlock;
+
 class Lz4
 {
 	static
 	function decompress(string $input) : string
 	{
-		return 'foobar';
+		return implode(
+			array_map(
+				[__CLASS__, 'decompressBlock',],
+				(new Lz4Frame($input))->dataBlocks() ) );
+	}
+
+	static
+	function decompressBlock(Lz4\DataBlock $DB) : string
+	{
+return 'foobar';
 	}
 }
