230be91ee6d465f321f3a03bedfe772d6889a9ab
dexen deVries <dexen.devries@gmail.com>
2020-01-21 14:38:04 +0100

    micro-optimization: use fewer variables
    yes it shaves off about 10% of execution time

--- src/Lz4.php
+++ src/Lz4.php
@@ -30,26 +30,24 @@ class Lz4
 
 				# operation A: a fragment of input $payload to be appended to output $ret
 				# NOP if $fieldA === 0
-			$fieldA = ($token >> 4) & 0xf;
-				# operation B: a fragment of output $ret to be appended to output $ret
-				# the encoding allows repeats via overlapping copy
-			$fieldB = ($token >> 0) & 0xf;
-
-			$numBytes = 0;
-			switch ($fieldA) {
+			$len = ($token >> 4) & 0xf;
+			switch ($len) {
 			case 15: # add some more bytes to indicate the full length
 				do {
-					$numBytes += ($nn = ord($payload[$pos++]));
+					$len += ($nn = ord($payload[$pos++]));
 				} while ($nn === 255);
 			default:	# a literal number of bytes
-				$numBytes += $fieldA;
-				$ret .= substr($payload, $pos, $numBytes);
-				$pos += $numBytes;
+				$ret .= substr($payload, $pos, $len);
+				$pos += $len;
 			case 0:	# no literal
 				; }
 
 				# https://github.com/lz4/lz4/blob/dev/doc/lz4_Block_format.md#end-of-block-restrictions
 			if ($pos < $blockSize) {
+					# operation B: a fragment of output $ret to be appended to output $ret
+					# the encoding allows repeats via overlapping copy
+				$len = ($token >> 0) & 0xf;
+
 					# the matchCopyOperation
 					# offset is encoded as little-endian uint16
 				$offset = ord($payload[$pos++]);
@@ -57,33 +55,30 @@ class Lz4
 				if ($offset === 0)
 					throw new MalformedLz4DataException('offset 0');
 
-				$minmatch = 4;
-				$matchlength = $minmatch;
-				switch ($fieldB) {
+				switch ($len) {
 				case 15:
 					do {
-						$matchlength += ($nn = ord($payload[$pos++]));
+						$len += ($nn = ord($payload[$pos++]));
 					} while ($nn === 255);
 				default:
-					$matchlength += $fieldB;
-				case 0:
-					; }
+						# minimum match len
+					$len += 4; }
 
 					# h/t to http://ticki.github.io/blog/how-lz4-works/
-				if ($matchlength <= $offset)
-					$ret .= substr($ret, -$offset, $matchlength);
+				if ($len <= $offset)
+					$ret .= substr($ret, -$offset, $len);
 				else {
 						# >which means that later bytes to copy are not yet decoded.
 						# >This is called an "overlap match", and must be handled with special care
 					$to_repeat = substr($ret, -$offset);
-					$num = (int)ceil(1.0 * $matchlength / $offset);
+					$num = (int)ceil(1.0 * $len / $offset);
 
 					$repeated = str_repeat($to_repeat, $num);
 
-					if (($num * $offset) === $matchlength)
+					if (($num * $offset) === $len)
 						$ret .= $repeated;
 					else
-						$ret .= substr($repeated, 0, $matchlength); } } }
+						$ret .= substr($repeated, 0, $len); } } }
 
 		return $ret;
 	}
