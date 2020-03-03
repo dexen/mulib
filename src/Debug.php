1c6b7467b29abecf8535eeaa896afa960365fd2f
dexen deVries <dexen.devries@gmail.com>
2020-03-03 16:41:36 +0100

    error reporting helpers

--- src/Debug.php
+++ src/Debug.php
@@ -62,4 +62,72 @@ class Debug
 				echo H($frame['line']);
 			echo PHP_EOL; }
 	}
+
+	static
+	function basicHtmlErrorHandler(int $errno, string $errstr, string $errfile, int $errline)
+	{
+		$H = fn(string $str) => print(htmlspecialchars($str));
+		$S = fn(...$a) => $H(sprintf(...$a));
+		$L = fn($fmt, ...$a) => $S($fmt .PHP_EOL, ...$a);
+
+		$E = function(int $errno) : string
+		{
+			return [
+				E_ERROR => 'ERROR',
+				E_WARNING => 'WARNING',
+				E_PARSE => 'PARSE ERROR',
+				E_NOTICE  => 'NOTICE',
+				E_CORE_ERROR => 'CORE ERROR',
+				E_CORE_WARNING => 'CORE WARNING',
+				E_COMPILE_ERROR => 'COMPILE ERROR',
+				E_COMPILE_WARNING => 'COMPILE WARNING',
+				E_USER_ERROR => 'USER ERROR',
+				E_USER_WARNING => 'USER WARNING',
+				E_USER_NOTICE => 'USER NOTICE',
+				E_STRICT => 'STRICT NOTICE',
+				E_RECOVERABLE_ERROR => 'RECOVERABLE ERROR',
+				E_DEPRECATED => 'DEPRECIATION WARNING',
+				E_USER_DEPRECATED => 'USER DEPRECIATION WARNING',
+			][$errno] ?? 'OTHER ERROR';
+		};
+
+		$P = [ __CLASS__, 'nicePhpScriptPathname' ];
+
+		echo '<pre>', PHP_EOL;
+
+		$L('%s: %s', $E($errno), $errstr);
+		$L('%s:%s', $P($errfile), $errline);
+
+		$a = debug_backtrace();
+		if ($a)
+			echo '--', PHP_EOL;
+		foreach ($a as $frame)
+			$L('	%s:%s', $P($frame['file']??null), $frame['line']??null);
+
+		echo '--', PHP_EOL;
+		die('Uncaught error, quitting.');
+	}
+
+	static
+	function basicHtmlExceptionHandler(\Throwable $T)
+	{
+		$H = fn(string $str) => print(htmlspecialchars($str));
+		$S = fn(...$a) => $H(sprintf(...$a));
+		$L = fn($fmt, ...$a) => $S($fmt .PHP_EOL, ...$a);
+
+		$P = [ __CLASS__, 'nicePhpScriptPathname' ];
+
+		echo '<pre>', PHP_EOL;
+
+		$L('%s: %s', get_class($T), $T->getMessage());
+		$L('%s:%s', $P($T->getFile()), $T->getLine());
+
+		if ($T->getTrace())
+			echo '--', PHP_EOL;
+		foreach ($T->getTrace() as $frame)
+			$L('	%s:%s', $P($frame['file']??null), $frame['line']??null);
+
+		echo '--', PHP_EOL;
+		die('Uncaught exception, quitting.');
+	}
 }
